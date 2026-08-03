143f2a4e16bae3e795da375fa52af9c8a1b053f6
25f1001475 <25f1001475@ds.study.iitm.ac.in>
2026-08-03 23:43:29 +0530

    Completed Milestone 3: Admin Dashboard and Management

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template,request,redirect,url_for,session
-from database import create_connection
-
+from database import (
+    create_connection,
+    get_dashboard_counts,
+    add_trek,
+    delete_trek,
+    get_all_treks,
+    get_trek_by_id,
+    update_trek,
+    get_all_staff,
+    approve_staff,
+    reject_staff,
+    get_approved_staff,
+    get_all_users,
+    deactivate_user,
+    activate_user,
+    search_records,
+    get_all_bookings,
+    deactivate_staff,
+    activate_staff
+)
 app = Flask(__name__)
 app.secret_key = "trekking_secret_key"
 
@@ -32,10 +50,15 @@ def login():
         stored_password = user[3]
         role = user[5]
         approval_status = user[6]
+        account_status = user[7]
 
         if password != stored_password:
             conn.close()
             return "Incorrect password."
+
+        if account_status == "Inactive":
+            conn.close()
+            return "Your account has been deactivated by the administrator."
 
         if role=="admin":
             session["user_id"] = user[0]
@@ -115,8 +138,14 @@ def register():
 def admin_dashboard():
     if "user_id" not in session or session["role"] != "admin":
             return redirect(url_for("login"))
-    return render_template("admin_dashboard.html")
-
+    total_treks, total_trekkers, total_staff, total_bookings = get_dashboard_counts()
+    return render_template(
+        "admin_dashboard.html",
+        total_treks=total_treks,
+        total_trekkers=total_trekkers,
+        total_staff=total_staff,
+        total_bookings=total_bookings
+    )
 
 @app.route("/staff_dashboard")
 def staff_dashboard():
@@ -130,6 +159,223 @@ def trekker_dashboard():
     if "user_id" not in session or session["role"] != "trekker":
             return redirect(url_for("login"))
     return render_template("trekker_dashboard.html")
+
+@app.route("/manage_treks")
+def manage_treks():
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+    
+    treks = get_all_treks()
+    return render_template(
+        "manage_treks.html",
+        treks=treks
+    )
+
+
+@app.route("/manage_staff")
+def manage_staff():
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    staff = get_all_staff()
+
+    return render_template(
+        "manage_staff.html",
+        staff=staff
+    )
+
+@app.route("/add_trek", methods=["GET", "POST"])
+def add_trek_page():
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+    if request.method == "POST":
+        trek_name = request.form["trek_name"]
+        location = request.form["location"]
+        difficulty = request.form["difficulty"]
+        duration = request.form["duration"]
+        start_date = request.form["start_date"]
+        end_date = request.form["end_date"]
+        available_slots = request.form["available_slots"]
+        assigned_staff = request.form["assigned_staff"]
+        status = request.form["status"]
+        description = request.form["description"]
+
+        add_trek(
+            trek_name,
+            location,
+            difficulty,
+            duration,
+            start_date,
+            end_date,
+            available_slots,
+            assigned_staff,
+            status,
+            description
+        )
+
+        return redirect(url_for("manage_treks"))
+    staff = get_approved_staff()
+    return render_template("add_trek.html",staff=staff)
+
+@app.route("/edit_trek/<int:trek_id>", methods=["GET", "POST"])
+def edit_trek_page(trek_id):
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    if request.method == "POST":
+        trek_name = request.form["trek_name"]
+        location = request.form["location"]
+        difficulty = request.form["difficulty"]
+        duration = request.form["duration"]
+        start_date = request.form["start_date"]
+        end_date = request.form["end_date"]
+        available_slots = request.form["available_slots"]
+        assigned_staff = request.form["assigned_staff"]
+        status = request.form["status"]
+        description = request.form["description"]
+
+        update_trek(
+            trek_id,
+            trek_name,
+            location,
+            difficulty,
+            duration,
+            start_date,
+            end_date,
+            available_slots,
+            assigned_staff,
+            status,
+            description
+        )
+
+        return redirect(url_for("manage_treks"))
+
+    trek = get_trek_by_id(trek_id)
+    staff = get_approved_staff()
+
+    return render_template(
+        "add_trek.html",
+        trek=trek,staff=staff
+    )
+
+@app.route("/delete_trek/<int:trek_id>")
+def delete_trek_page(trek_id):
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+    
+    delete_trek(trek_id)
+
+    return redirect(url_for("manage_treks"))
+
+@app.route("/approve_staff/<int:user_id>")
+def approve_staff_page(user_id):
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    approve_staff(user_id)
+
+    return redirect(url_for("manage_staff"))
+
+@app.route("/reject_staff/<int:user_id>")
+def reject_staff_page(user_id):
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    reject_staff(user_id)
+
+    return redirect(url_for("manage_staff"))
+
+@app.route("/deactivate_staff/<int:user_id>")
+def deactivate_staff_page(user_id):
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    deactivate_staff(user_id)
+
+    return redirect(url_for("manage_staff"))
+
+
+@app.route("/activate_staff/<int:user_id>")
+def activate_staff_page(user_id):
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    activate_staff(user_id)
+
+    return redirect(url_for("manage_staff"))
+
+@app.route("/manage_users")
+def manage_users():
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    users = get_all_users()
+
+    return render_template(
+        "manage_users.html",
+        users=users
+    )
+@app.route("/deactivate_user/<int:user_id>")
+def deactivate_user_page(user_id):
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    deactivate_user(user_id)
+
+    return redirect(url_for("manage_users"))
+
+
+@app.route("/activate_user/<int:user_id>")
+def activate_user_page(user_id):
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    activate_user(user_id)
+
+    return redirect(url_for("manage_users"))
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    results = []
+
+    if request.method == "POST":
+
+        keyword = request.form["keyword"]
+
+        results = search_records(keyword)
+
+    return render_template(
+        "search.html",
+        results=results
+    )
+
+@app.route("/booking_records")
+def booking_records():
+
+    if "user_id" not in session or session["role"] != "admin":
+        return redirect(url_for("login"))
+
+    bookings = get_all_bookings()
+
+    return render_template(
+        "booking_records.html",
+        bookings=bookings
+    )
 
 @app.route("/logout")
 def logout():
--- database.py
+++ database.py
@@ -12,7 +12,7 @@ def create_tables():
     cursor.execute("PRAGMA foreign_keys = ON")
 
     cursor.execute(
-        """ 
+    """ 
 CREATE TABLE IF NOT EXISTS Users(
             user_id INTEGER PRIMARY KEY AUTOINCREMENT,
             email TEXT NOT NULL UNIQUE,
@@ -20,25 +20,27 @@ CREATE TABLE IF NOT EXISTS Users(
             password TEXT NOT NULL,
             phone TEXT,
             role TEXT NOT NULL DEFAULT 'user',
-            approval_status TEXT NOT NULL DEFAULT 'Approved'
+            approval_status TEXT NOT NULL DEFAULT 'Approved',
+            account_status TEXT NOT NULL DEFAULT 'Active'
         )
 """
-    )
+)
 
     cursor.execute(
         """
-CREATE TABLE IF NOT EXISTS Treks (
-            trek_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            trek_name TEXT NOT NULL,
-            location TEXT NOT NULL,
-            difficulty TEXT NOT NULL,
-            duration INTEGER NOT NULL,
-            available_slots INTEGER NOT NULL,
-            assigned_staff_id INTEGER NOT NULL,
-            start_date TEXT NOT NULL,
-            end_date TEXT NOT NULL,
-            status TEXT NOT NULL DEFAULT 'Open',
-            FOREIGN KEY (assigned_staff_id) REFERENCES Users(user_id)
+CREATE TABLE IF NOT EXISTS Treks(
+    trek_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    trek_name TEXT NOT NULL,
+    location TEXT NOT NULL,
+    difficulty TEXT NOT NULL,
+    duration INTEGER NOT NULL,
+    start_date TEXT NOT NULL,
+    end_date TEXT NOT NULL,
+    available_slots INTEGER NOT NULL,
+    assigned_staff INTEGER,
+    status TEXT NOT NULL DEFAULT 'Open',
+    description TEXT,
+    FOREIGN KEY (assigned_staff) REFERENCES Users(user_id)
         )
         """
     )
@@ -76,23 +78,418 @@ def create_admin():
 
     if admin is None:
         cursor.execute("""
-            INSERT INTO Users
-            (email, name, password, phone, role, approval_status)
-            VALUES (?, ?, ?, ?, ?, ?)
-        """, (
-            "admin@trek.com",
-            "Admin",
-            "admin123",
-            "9876543210",
-            "admin",
-            "Approved"
-        ))
+    INSERT INTO Users
+    (email, name, password, phone, role, approval_status, account_status)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+""", (
+    "admin@trek.com",
+    "Admin",
+    "admin123",
+    "9876543210",
+    "admin",
+    "Approved",
+    "Active"
+))
 
         conn.commit()
         print("Default admin created.")
 
     else:
         print("Admin already exists.")
+
+def get_dashboard_counts():
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM Treks
+    """)
+    total_treks = cursor.fetchone()[0]
+
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM Users
+        WHERE role = 'trekker'
+    """)
+    total_trekkers = cursor.fetchone()[0]
+
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM Users
+        WHERE role = 'staff'
+    """)
+    total_staff = cursor.fetchone()[0]
+
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM Bookings
+    """)
+    total_bookings = cursor.fetchone()[0]
+
+    conn.close()
+
+    return (
+        total_treks,
+        total_trekkers,
+        total_staff,
+        total_bookings
+    )
+
+def add_trek(trek_name, location, difficulty, duration,
+             start_date, end_date, available_slots,assigned_staff, status,
+             description):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO Treks
+        (trek_name, location, difficulty, duration,
+         start_date, end_date, available_slots,assigned_staff,
+         status, description)
+
+         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (
+        trek_name,
+        location,
+        difficulty,
+        duration,
+        start_date,
+        end_date,
+        available_slots,
+        assigned_staff,
+        status,
+        description
+    ))
+
+    conn.commit()
+    conn.close()
+
+def delete_trek(trek_id):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        DELETE FROM Treks
+        WHERE trek_id = ?
+    """, (trek_id,))
+
+    conn.commit()
+    conn.close()
+
+def get_all_treks():
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT
+    T.trek_id,
+    T.trek_name,
+    T.location,
+    T.difficulty,
+    U.name,
+    T.available_slots,
+    T.status
+    FROM Treks T
+    LEFT JOIN Users U
+    ON T.assigned_staff = U.user_id
+    ORDER BY T.trek_id
+    """)
+
+    treks = cursor.fetchall()
+
+    conn.close()
+
+    return treks
+
+def get_trek_by_id(trek_id):
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT *
+        FROM Treks
+        WHERE trek_id = ?
+    """, (trek_id,))
+
+    trek = cursor.fetchone()
+
+    conn.close()
+    return trek
+
+def update_trek(trek_id, trek_name, location, difficulty,
+                duration, start_date, end_date,
+                available_slots,assigned_staff,status, description):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE Treks
+        SET trek_name = ?,
+            location = ?,
+            difficulty = ?,
+            duration = ?,
+            start_date = ?,
+            end_date = ?,
+            available_slots = ?,
+            assigned_staff = ?,
+            status = ?,
+            description = ?
+        WHERE trek_id = ?
+    """, (
+        trek_name,
+        location,
+        difficulty,
+        duration,
+        start_date,
+        end_date,
+        available_slots,
+        assigned_staff,
+        status,
+        description,
+        trek_id
+    ))
+
+    conn.commit()
+    conn.close()
+
+def get_all_staff():
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT user_id,
+               name,
+               email,
+               phone,
+               approval_status,
+               account_status
+        FROM Users
+        WHERE role = 'staff'
+    """)
+
+    staff = cursor.fetchall()
+
+    conn.close()
+
+    return staff
+
+def approve_staff(user_id):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE Users
+        SET approval_status = 'Approved'
+        WHERE user_id = ?
+    """, (user_id,))
+
+    conn.commit()
+    conn.close()
+
+def reject_staff(user_id):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        DELETE FROM Users
+        WHERE user_id = ?
+    """, (user_id,))
+
+    conn.commit()
+    conn.close()
+
+def get_approved_staff():
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT user_id, name
+        FROM Users
+        WHERE role = 'staff'
+        AND approval_status = 'Approved'
+    """)
+
+    staff = cursor.fetchall()
+
+    conn.close()
+
+    return staff
+
+def deactivate_staff(user_id):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE Users
+        SET account_status = 'Inactive'
+        WHERE user_id = ?
+    """, (user_id,))
+
+    conn.commit()
+    conn.close()
+
+
+def activate_staff(user_id):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE Users
+        SET account_status = 'Active'
+        WHERE user_id = ?
+    """, (user_id,))
+
+    conn.commit()
+    conn.close()
+
+def get_all_users():
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT
+            user_id,
+            name,
+            email,
+            phone,
+            role,
+            approval_status,
+            account_status
+        FROM Users
+        ORDER BY user_id
+    """)
+
+    users = cursor.fetchall()
+
+    conn.close()
+
+    return users
+
+def deactivate_user(user_id):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE Users
+        SET account_status = 'Inactive'
+        WHERE user_id = ?
+    """, (user_id,))
+
+    conn.commit()
+    conn.close()
+
+
+def activate_user(user_id):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE Users
+        SET account_status = 'Active'
+        WHERE user_id = ?
+    """, (user_id,))
+
+    conn.commit()
+    conn.close()
+
+def search_records(keyword):
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    keyword = f"%{keyword}%"
+
+    results = []
+
+    # Search Treks
+    cursor.execute("""
+        SELECT
+            trek_id,
+            trek_name
+        FROM Treks
+        WHERE trek_name LIKE ?
+           OR CAST(trek_id AS TEXT) LIKE ?
+    """, (keyword, keyword))
+
+    for row in cursor.fetchall():
+        results.append(("Trek", row[0], row[1]))
+
+    # Search Staff
+    cursor.execute("""
+        SELECT
+            user_id,
+            name
+        FROM Users
+        WHERE role='staff'
+        AND (
+            name LIKE ?
+            OR CAST(user_id AS TEXT) LIKE ?
+        )
+    """, (keyword, keyword))
+
+    for row in cursor.fetchall():
+        results.append(("Staff", row[0], row[1]))
+
+    # Search Trekkers
+    cursor.execute("""
+        SELECT
+            user_id,
+            name
+        FROM Users
+        WHERE role='trekker'
+        AND (
+            name LIKE ?
+            OR CAST(user_id AS TEXT) LIKE ?
+        )
+    """, (keyword, keyword))
+
+    for row in cursor.fetchall():
+        results.append(("User", row[0], row[1]))
+
+    conn.close()
+
+    return results
+
+def get_all_bookings():
+
+    conn = create_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT
+            B.booking_id,
+            U.name,
+            T.trek_name,
+            B.booking_date,
+            B.status
+        FROM Bookings B
+        JOIN Users U
+            ON B.user_id = U.user_id
+        JOIN Treks T
+            ON B.trek_id = T.trek_id
+        ORDER BY B.booking_id
+    """)
+
+    bookings = cursor.fetchall()
+
+    conn.close()
+
+    return bookings
 
 if __name__ == "__main__":
     create_tables()
